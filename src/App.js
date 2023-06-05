4fa26685154d84525d6fac428b9c1ee7d2fe64e9
Lucinde <lucinde@lucinde.nl>
2023-06-05 13:22:31 +0200

    Add styling for Header - no functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import Header from "./components/header/Header";
 
-/*** import images **/
+/*** import icons **/
+import ico_next from "./assets/icons/arrow_next.svg";
+import ico_prev from "./assets/icons/arrow_previous.svg";
 import ico_customers from "./assets/icons/customers.svg";
+import ico_customers_add from "./assets/icons/customers-add.svg";
+import ico_dashboard from "./assets/icons/dashboard.svg";
+import ico_delete from "./assets/icons/delete.svg";
+import ico_logout from "./assets/icons/logout.svg";
+import ico_mechanic from "./assets/icons/mechanic.svg";
+import ico_planning from "./assets/icons/planning.svg";
+import ico_profile from "./assets/icons/profile.svg";
+import ico_tasks_add from "./assets/icons/task-add.svg";
+import ico_tasks from "./assets/icons/tasks.svg";
 
 function App() {
   return (
     <>
-      <Header ico_customers={ico_customers}/>
+      <Header
+          ico_customers_add={ico_customers_add}
+          ico_tasks_add={ico_tasks_add}
+          ico_profile={ico_profile}
+          ico_logout={ico_logout}
+      />
     </>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,17 +3,20 @@ import './Header.css';
 
 // import ico_customers from "././assets/icons/customers.svg";
 
-function Header({ico_customers}) {
+function Header({ico_customers_add, ico_tasks_add, ico_profile, ico_logout}) {
     return (
         <header>
             <div className="outer-container">
                 <div className="inner-container flex-row">
-                    <h4>PlannerPro</h4>
+                    <h4>Planner<span className="h4-light"> Pro</span></h4>
                     <nav className="quicknav">
                         <ul>
-                            <li>Hier komt de navigatie</li>
-                            <li><img alt="icon customers" src={ico_customers} className="icon"/></li>
+                            <li><img alt="icon customers" src={ico_customers_add} className="icon"/></li>
+                            <li><img alt="icon add task" src={ico_tasks_add} className="icon"/></li>
+                            <li><img alt="icon profile" src={ico_profile} className="icon"/></li>
+                            <li><img alt="icon logout" src={ico_logout} className="icon"/></li>
                         </ul>
+                        <span>Planner naam</span>
                     </nav>
                 </div>
             </div>
