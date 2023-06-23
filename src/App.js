72b73993437d418fc38791d544ecf7774d5f42d8
Lucinde <lucinde@lucinde.nl>
2023-06-23 13:54:51 +0200

    Remove header and footer for not-authenticated users

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Footer from "./components/footer/Footer";
 import PlannerHome from "./pages/planner/PlannerHome";
 import MechanicHome from "./pages/mechanic/MechanicHome";
 import Login from "./pages/login/Login";
+import {useContext} from "react";
+import {AuthContext} from "./context/AuthContext";
 
 function App() {
+    const {isAuth} = useContext(AuthContext);
     return (
         <>
             {/*todo: bepalen of de header en footer met een authorisatie van de pagina's op display: none wordt gezet bij het inloggen of op elke pagina geïmporteerd moet worden*/}
-            <Header/>
+            {isAuth && <Header/> }
             <Routes>
                 {/*todo: Voorwaarde toevoegen waarin de homepage geredirect wordt naar de planner/mechanic home indien je ingelogd bent, zie aantekeningen routing*/}
                 <Route path="/" element={<Login/>}/>
                 <Route path="/planner" element={<PlannerHome/>}/>
                 <Route path="/mechanic" element={<MechanicHome/>}/>
             </Routes>
-            <Footer/>
+            {isAuth && <Footer/> }
         </>
     );
 }
