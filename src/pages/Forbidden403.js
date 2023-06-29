6b955a461f7ed4f06d75c957e82e8f746b39eca5
Lucinde <lucinde@lucinde.nl>
2023-06-29 11:11:49 +0200

    Add 403-forbidden page

---
+++ src/pages/Forbidden403.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Forbidden403(props) {
+    return (
+        <main className="forbidden-403">
+            <div className="outer-container">
+                <div className="inner-container">
+                    <h1>Sorry!</h1>
+                    <p>Je bent niet gemachtigd deze pagina te bekijken! Neem contact op met de beheerder via
+                        admin@plannerpro.nl als je denkt dat je deze pagina wel nodig hebt.</p>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Forbidden403;
