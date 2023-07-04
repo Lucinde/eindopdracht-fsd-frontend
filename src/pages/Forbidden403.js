6ad831b4453fd83fded72ccac3ac2ec06343ad64
Lucinde <lucinde@lucinde.nl>
2023-07-04 11:26:21 +0200

    Add admin-email to 403-page

--- src/pages/Forbidden403.js
+++ src/pages/Forbidden403.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import configData from "../config.json";
 
 function Forbidden403(props) {
+    const adminEmail = `${configData.ADMIN_EMAIL}`;
+
     return (
         <main className="forbidden-403">
             <div className="outer-container">
                 <div className="inner-container">
                     <h1>Sorry!</h1>
                     <p>Je bent niet gemachtigd deze pagina te bekijken!</p>
-                    <p> Neem contact op met de beheerder via admin@plannerpro.nl als je denkt dat je deze pagina wel
+                    <p> Neem contact op met de beheerder via <a href={`mailto:${adminEmail}`}>{adminEmail}</a> als je denkt dat je deze pagina wel
                         nodig hebt.</p>
                 </div>
             </div>
