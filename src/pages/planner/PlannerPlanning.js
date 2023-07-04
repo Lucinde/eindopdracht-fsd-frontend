1e2da18748efd0741941a3b441309e88330aad4c
Lucinde <lucinde@lucinde.nl>
2023-07-04 14:54:40 +0200

    Add component for PlannerPlanning

---
+++ src/pages/planner/PlannerPlanning.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PlannerPlanning(props) {
+    return (
+        <main>
+            <h1>Hier komt de planning</h1>
+        </main>
+    );
+}
+
+export default PlannerPlanning;
