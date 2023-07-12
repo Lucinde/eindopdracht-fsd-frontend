c37f5e57b80c6d583a85a27910bae74bbab518bd
Lucinde <lucinde@lucinde.nl>
2023-07-12 09:59:17 +0200

    Add validation rules and error messages to AddNewCustomer

--- src/components/forms/AddNewCustomer.js
+++ src/components/forms/AddNewCustomer.js
@@ -1,12 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {useForm} from "react-hook-form";
 import axios from "axios";
 import configData from "../../config.json";
 import FormInput from "./FormInput";
 import Button from "../buttons/Button";
 import "./Forms.css";
+import {IconContext} from "../../context/IconContext";
 
 function AddNewCustomer({closeModal, handleUpdate}) {
+    const {ico_warning} = useContext(IconContext);
     const {register, handleSubmit, setValue, formState: {errors}} = useForm();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
@@ -31,8 +33,7 @@ function AddNewCustomer({closeModal, handleUpdate}) {
             }
             closeModal();
         } catch (e) {
-            console.error("Hier gaat iets mis!" + e);
-            // todo: error handling in UI weergeven!
+            setError(e.response.data);
         } finally {
             setLoading(false);
         }
@@ -46,22 +47,33 @@ function AddNewCustomer({closeModal, handleUpdate}) {
                     <FormInput inputType="text" name="firstName" register={register}
                                errors={errors}>Voornaam: </FormInput>
                     <FormInput inputType="text" name="lastName" register={register}
+                               validationSchema={{required: "Vul een achternaam in"}}
                                errors={errors}>Achternaam: </FormInput>
                     <FormInput inputType="text" name="address" register={register}
                                errors={errors}>Adres: </FormInput>
                     <FormInput inputType="text" name="zip" register={register}
                                errors={errors}>Postcode: </FormInput>
                     <FormInput inputType="text" name="city" register={register}
+                               validationSchema={{required: "Vul een woonplaats in"}}
                                errors={errors}>Woonplaats: </FormInput>
                     <FormInput inputType="text" name="phoneNumber" register={register}
+                               validationSchema={{required: "Vul een telefoonnummer in"}}
                                errors={errors}>Telefoonnummer: </FormInput>
                     <FormInput inputType="text" name="email" register={register}
+                               validationSchema={{required: "Vul een e-mailadres in", pattern: {
+                                       value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                       message: "Ongeldig e-mailadres"
+                                   }}}
                                errors={errors}>E-mail: </FormInput>
                 </div>
                 <div className="button-wrapper right">
                     <Button variant="secondary" type="reset" handleClick={closeModal}>Annuleren</Button>
                     <Button variant="primary" type="submit">Klant opslaan</Button>
                 </div>
+                {error &&
+                    <p className="text-error"><img src={ico_warning} alt="icon details"
+                                                   className="icon warning"/> {error}</p>
+                }
             </form>
         </article>
     );
